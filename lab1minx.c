ed79b662e5ed85bb17bfc0c9f02cdfc33c8eac46
6704062617156 <s6704062617156@email.kmutnb.ac.th>
2024-11-28 18:18:52 +0700

    the upgrade of the lastest version, v0.0.4 released

--- lab1minx.c
+++ lab1minx.c
@@ -3,10 +3,7 @@
 int main() {
 	int n1,n2;
 	
-	printf("n1=");
-	scanf("%d",&n1);
-	printf("n2=");
-	scanf("%d",&n2);
+	scanf("%d %d", &n1, &n2);
 	
 	if(n1>n2) {
 		printf("num1 is greater than num2\n");
